4c4b60350897f94b43477a9f281a0f5f27dd6565
Antti Vahalummukka <vahisa@gmail.com>
2013-07-13 01:43:22 +0300

    Now uses buf.toString('utf-8')

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var fs = require('fs');
 
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var r = fs.readFileSync("index.html");
+  var buf = new Buffer(r, "utf-8");
+  
   //response.send('Hello World 2!');
-  response.send(r);
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
